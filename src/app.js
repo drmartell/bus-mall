6b1cb70403b28f3c61530650e4eff85d51867ca9
503-888-1044 Martell <drmartell@gmail.com>
2019-10-15 12:41:52 -0700

    adds local storage of aggregate sessions

--- src/app.js
+++ src/app.js
@@ -1,34 +1,39 @@
 import { productsArr } from '../data/products-api.js';
 
+const ALL_TIME_KEY = 'allSales';
 const productsArrCopy = productsArr.slice();
 
-const productsIdArr = productsArr.map(product => product.id); // lets see if can work with mutable array of id's only
+const productsIdArr = productsArr.map(product => product.id);
 
-let numProducts = 3; // will allow for this to be user selected ultimately
-const requiredSelections = 1;
+let numProducts = 3;
+const requiredSelections = 3;
+
+let selectionsMade = 0;
+
+const imgsSection = document.getElementById('imgs-section');
+const progressDiv = document.getElementById('progress-text-id');
+
+progressDiv.textContent = `You are on page 1 of ${requiredSelections}`;
 
 let randomProductsIdsArr = [];
 
 for (let i = 0; i < numProducts; i++) {
-    randomProductsIdsArr.push(''); // prepopulate array to prevent incrementing of shownCounts of items that wouldn't be shown on initial pass
+    randomProductsIdsArr.push('');
 }
 
 const getRandomId = (max) => Math.ceil(Math.random() * max);
 
-const trimProductsArr = () => randomProductsIdsArr = randomProductsIdsArr.slice(numProducts, numProducts * 2); // remove front half of array to prep for next additions
-//I won't need this to remain a function if I call it immediately, not sure yet
+const trimProductsArr = () => randomProductsIdsArr = randomProductsIdsArr.slice(numProducts, numProducts * 2);
 function getRandomProducts(numRandomProducts) {
-    while (randomProductsIdsArr.length < (numRandomProducts * 2)) { // double this array length to retain the 'prior' set of randoms
+    while (randomProductsIdsArr.length < (numRandomProducts * 2)) {
         let thisRandomId = getRandomId(productsIdArr.length);
         const thisProduct = getProductById(thisRandomId);
         if (!randomProductsIdsArr.includes(thisRandomId)) {
             randomProductsIdsArr.push(thisRandomId);
-            thisProduct.shownCount = thisProduct.shownCount ? thisProduct.shownCount + 1 : 1; // initialize / increment number of times shown
+            thisProduct.shownCount = thisProduct.shownCount ? thisProduct.shownCount + 1 : 1;
         }
     }
 } getRandomProducts(numProducts);
-
-//console.log(randomProductsIdsArr);
 
 function getProductById(productId) {
     for (let product of productsArrCopy)
@@ -36,36 +41,28 @@ function getProductById(productId) {
             return product;
 }
 
-const imgsSection = document.getElementById('imgs-section');
-
 function showImages() {
     for (let i = 0; i < numProducts; i++) {
         const imgTag = document.createElement('img');
         const thisProduct = getProductById(randomProductsIdsArr[i + numProducts]);
-        //console.log(randomProductsIdsArr[i + numProducts]);
         imgTag.src = thisProduct.imageurl;
         imgTag.alt = thisProduct.name;
         imgTag.id = thisProduct.id;
         imgTag.addEventListener('click', (e) => handleSelection(e.target.id));
         imgsSection.appendChild(imgTag);
     }
-    //console.log(randomProductsIdsArr);
     trimProductsArr();
-    //console.log(randomProductsIdsArr);
 } showImages();
 
 function removeImgTags() {
     imgsSection.innerHTML = '';
 }
 
-let selectionsMade = 0; // results displayed after minimum of 25 selections
-
 function handleSelection(imgIdString) {
     selectionsMade++;
     const imgId = Number(imgIdString);
     const thisProduct = getProductById(imgId);
-    thisProduct.selectedCount = thisProduct.selectedCount ? thisProduct.selectedCount + 1 : 1; // initialize / increment number of times selected
-    alert(`you clicked ${JSON.stringify(thisProduct)}`);
+    thisProduct.selectedCount = thisProduct.selectedCount ? thisProduct.selectedCount + 1 : 1;
     if (selectionsMade === requiredSelections) {
         showResults();
     }
@@ -73,25 +70,79 @@ function handleSelection(imgIdString) {
         removeImgTags();
         getRandomProducts(numProducts);
         showImages();
+        progressDiv.textContent = `You are on page ${selectionsMade + 1} of ${requiredSelections}`;
     }
 }
 
 function showResults() { 
+    progressDiv.textContent = `You are on the results page`;
     removeImgTags();
     renderResultsList();
+    saveToAllTime();
 }
 
-function renderResultsList() {
-    console.log(productsArrCopy);
+function renderResultsList(alltime = false) {
+
     const ulTag = document.createElement('ul');
-    imgsSection.appendChild(ulTag);
-    for (let i = 0; i < productsArrCopy.length; i++) {
-        let thisProduct = productsArrCopy[i];
-        if (thisProduct.shownCount) {
-            const liTag = document.createElement('li');
-            const thisSelectedCount = thisProduct.selectedCount ? thisProduct.selectedCount : 0;
-            liTag.textContent = `${thisProduct.name} was show ${thisProduct.shownCount} times, and chosen ${(thisSelectedCount / thisProduct.shownCount) * 100}% of the times it was shown.`;
-            ulTag.appendChild(liTag);
+    if (!alltime) {
+        imgsSection.appendChild(ulTag);
+        productsArrCopy.sort((a, b) => (a.name > b.name) ? 1 : -1);
+        for (let i = 0; i < productsArrCopy.length; i++) {
+            let thisProduct = productsArrCopy[i];
+            if (thisProduct.shownCount) {
+                const liTag = document.createElement('li');
+                const thisSelectedCount = thisProduct.selectedCount ? thisProduct.selectedCount : 0;
+                liTag.textContent = `${thisProduct.name} was show ${thisProduct.shownCount} times, and chosen ${(thisSelectedCount / thisProduct.shownCount) * 100}% of the times it was shown in this round.`;
+                ulTag.appendChild(liTag);
+            }
         }
     }
+    else {
+        const h2Tag = document.createElement('h2');
+        h2Tag.textContent = `Overall Findings`;
+        imgsSection.appendChild(h2Tag);
+        imgsSection.appendChild(ulTag);
+        const dataStore = getDataStore();
+        dataStore.sort((a, b) => (a.name > b.name) ? 1 : -1);
+        for (let i = 0; i < dataStore.length; i++) {
+            let thisSavedProduct = dataStore[i];
+            if (thisSavedProduct.shownCount) {
+                const liTag = document.createElement('li');
+                const thisSelectedCount = thisSavedProduct.selectedCount ? thisSavedProduct.selectedCount : 0;
+                liTag.textContent = `${thisSavedProduct.name} was show ${thisSavedProduct.shownCount} times, and chosen ${(thisSelectedCount / thisSavedProduct.shownCount) * 100}% of the times it was shown overall.`;
+                ulTag.appendChild(liTag);
+            }
+        }
+    }
+}
+
+function getDataStore() {
+    if (!localStorage.getItem(ALL_TIME_KEY)) localStorage.setItem(ALL_TIME_KEY, '[]');
+    return JSON.parse(localStorage.getItem(ALL_TIME_KEY));
+}
+
+function saveToAllTime() {
+    const dataStore = getDataStore();
+    const productsShown = productsArrCopy.filter(product => product.shownCount > 0);
+    if (dataStore.length === 0) localStorage.setItem(ALL_TIME_KEY, JSON.stringify(productsShown));
+    else {
+        for (let product of productsShown) {
+            let productInDataStore = false;
+            for (let storedProduct of dataStore) {
+                if (product.id === storedProduct.id) {
+                    console.log('found product');
+                    storedProduct.shownCount += product.shownCount;
+                    storedProduct.selectedCount += product.selectedCount;
+                    productInDataStore = true;
+                    break;
+                }
+            }
+            if (!productInDataStore) {
+                console.log('needs to be added to store');
+                dataStore.push(product);
+                localStorage.setItem(ALL_TIME_KEY, JSON.stringify(dataStore));
+            }
+        }
+    }
+    renderResultsList(true);
 }
